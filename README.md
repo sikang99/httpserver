672943b77a7f93d06b1988820f72e8456fba448d
Stoney Kang <sikang99@gmail.com>
2015-04-21 21:38:14 +0900

    add HTTPS service

--- README.md
+++ README.md
@@ -27,6 +27,7 @@ To serve a different directory use the `-root` flag:
 
 ## References
 
+- [Golang. What to use? http.ServeFile(..) or http.FileServer(..)?](http://stackoverflow.com/questions/28793619/golang-what-to-use-http-servefile-or-http-fileserver)
 - [HTTPS and Go](https://www.kaihag.com/https-and-go/)
 - [Static http file server in Go](https://www.chrismytton.uk/2013/07/17/golang-static-http-file-server/)
 - Forked from [chrismytton/httpserver](https://github.com/chrismytton/httpserver).
--- httpserver.go
+++ httpserver.go
@@ -2,21 +2,30 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"sync"
 )
 
 var (
-	port  = flag.String("port", "8000", "Define what TCP port to bind to")
-	sport = flag.String("sport", "8001", "Define what TCP port to bind to")
-	root  = flag.String("root", ".", "Define the root filesystem path")
+	port    = flag.String("port", "8000", "Define what TCP port to bind to")
+	sport   = flag.String("sport", "8001", "Define what TCP port to bind to")
+	root    = flag.String("root", ".", "Define the root filesystem path")
+	version = flag.String("version", "0.1.0", "Version number")
 )
 
 var wg sync.WaitGroup
 
 func main() {
 	flag.Parse()
+
+	http.HandleFunc("/hello", helloHandler)
+	//http.HandleFunc("/static", fileHandler)
+	http.Handle("/static", http.FileServer(http.Dir("./static/")))
+
+	//fs := http.FileServer(http.Dir("./static"))
+	//http.Handle("/static", http.StripPrefix("/static", fs))
 
 	wg.Add(1)
 	go serveHttp()
@@ -29,16 +38,27 @@ func main() {
 
 func serveHttp() {
 	defer wg.Done()
-	log.Println("Starting web server at http://0.0.0.0:" + *port)
-	http.ListenAndServe(":"+*port, fileHandler())
+	log.Println("Starting HTTP server at http://127.0.0.1:" + *port)
+	//http.ListenAndServe(":"+*port, fileServer())
+	http.ListenAndServe(":"+*port, nil)
 }
 
 func serveHttps() {
 	defer wg.Done()
-	log.Println("Starting web server at https://0.0.0.0:" + *sport)
-	http.ListenAndServeTLS(":"+*sport, "cert.pem", "key.pem", fileHandler())
+	log.Println("Starting HTTPS server at https://127.0.0.1:" + *sport)
+	//http.ListenAndServeTLS(":"+*sport, "cert.pem", "key.pem", fileServer())
+	http.ListenAndServeTLS(":"+*sport, "cert.pem", "key.pem", nil)
 }
 
-func fileHandler() http.Handler {
+func fileServer() http.Handler {
 	return http.FileServer(http.Dir(*root))
+}
+
+func fileHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL.Path)
+	http.ServeFile(w, r, r.URL.Path[1:])
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there!")
 }
